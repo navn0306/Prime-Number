e44825b173959266bf93565cb2267efa55118efd
Navneet <navn0306@gmail.com>
2021-11-29 15:41:06 +0530

    ADD: Added the welcome message for projet

---
+++ src/Prime/com/bridge/prime.java
@@ -0,0 +1,7 @@
+package Prime.com.bridge;
+
+public class prime {
+    public static void main(String[] args) {
+        System.out.println("\nWelcome, this program checks if the given integer input is Prime or not");
+    }
+}
